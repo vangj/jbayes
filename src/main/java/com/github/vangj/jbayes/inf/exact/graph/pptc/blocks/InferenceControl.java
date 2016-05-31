22c1a340e3388f3fb0e68c31014ee802a46a2596
Jee Vang <vangjee@gmail.com>
2016-05-31 00:32:54 -0400

    add javadocs

--- src/main/java/com/github/vangj/jbayes/inf/exact/graph/pptc/blocks/InferenceControl.java
+++ src/main/java/com/github/vangj/jbayes/inf/exact/graph/pptc/blocks/InferenceControl.java
@@ -9,13 +9,22 @@ import static com.github.vangj.jbayes.inf.exact.graph.pptc.blocks.Moralize.*;
 import static com.github.vangj.jbayes.inf.exact.graph.pptc.blocks.Triangulate.*;
 import static com.github.vangj.jbayes.inf.exact.graph.pptc.blocks.Transform.*;
 
+/**
+ * Controls inference based on evidence.
+ */
 public class InferenceControl implements JoinTree.Listener {
 
+  /**
+   * Creates a join tree from a DAG with inference enabled.
+   * @param dag DAG.
+   * @return Join tree.
+   */
   public static JoinTree apply(Dag dag) {
     JoinTree joinTree = propagate(initialization(transform(triangulate(moralize(dag)))))
         .setListener(new InferenceControl());
     return joinTree;
   }
+
   @Override
   public void evidenceRetracted(JoinTree joinTree) {
     initialization(joinTree);
